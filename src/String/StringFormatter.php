a1e51553ea904905b4ff3ca897f2c7e415386872
Charles <charles.tardy@hotmail.com>
2017-07-05 10:38:53 +0200

    structur of project

---
+++ src/String/StringFormatter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace String;
+
+class StringFormatter
+{
+    
+    private $string;
+    
+    public function __construct(string $string) {
+        $this->string = $string;
+    }
+    
+    public function concat(string $str1, string $str2) {
+        
+    }
+    
+    public function toCamelCase(string $str1, string $str2) {
+        
+    }
+    
+    
+    public function prefix(string $str, string $pref, bool $toCamelCase=false) {
+        
+    }
+    
+    public function suffix(string $str, string $suf, bool $toCamelCase=false) {
+        
+    }
+    
+}
+
+
+?>
